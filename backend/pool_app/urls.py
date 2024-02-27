09f6d3fc2c7fdcabc4ef5593c2dad2461b1d7d96
codarsssss <codarsssss@gmail.com>
2024-02-27 22:39:22 +0300

    текущий результат

--- backend/pool_app/urls.py
+++ backend/pool_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('lessons/', lesson_list, name='lesson_list'),
     path('instructors/<int:pk>/', instructor_detail, name='instructor_detail'),
     path('lessons/<int:pk>/', lesson_detail, name='lesson_detail'),
-    # path('instructors/<int:instructor_id>/add_review/', add_instructor_review,
-    #      name='add_instructor_review'),
+    path('instructors/<int:instructor_id>/add_review/', add_instructor_review,
+         name='add_instructor_review'),
     # path('add_pool_review/', add_pool_review, name='add_pool_review'),
     path('review/', review_list, name='review_list'),
     path('instructor-review/', instructor_review_list, name='instructor_review_list')
